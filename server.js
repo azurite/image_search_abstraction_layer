6f2cf9d9e0fac5fe40bb386f203c01057c48ee30
Marko Nikic <darkblue.azurite@gmail.com>
2016-10-04 19:10:01 +0200

    finished basic setup

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
+var logger = require("connect-logger");
+var errorhandler = require("errorhandler");
+var favicon = require("serve-favicon");
+var _static = require("serve-static");
+
+var url = require("url");
+var qs = require("querystring");
 var http = require("http");
+var path = require("path");
+
+var express = require("express");
+var app = express();
+
+if(app.get("env") === "development") {
+  app.use(logger());
+  app.use(errorhandler());
+}
+
+app.use(favicon(path.join(__dirname, "/public/favicon.ico")));
+app.use(_static(path.join(__dirname, "/public")));
+
+http.createServer(app).listen(process.env.PORT || 8080);
